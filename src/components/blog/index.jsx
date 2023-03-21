96cc7d5f9b64fd705923a0d96a864af3d77e56f1
Sherief Elsowiny <elsowiny@yahoo.com>
2023-03-20 23:36:05 -0400

    add vite and pnpm
    Co-authored-by: Aiman <73438160+Aimanx9@users.noreply.github.com>

--- src/components/blog/index.jsx
+++ src/components/blog/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { sanitize } from 'dompurify'
+import { sanitize } from 'isomorphic-dompurify'
 import ScrollToTop from 'react-scroll-up'
 import { FiChevronUp } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
---
+++ src/components/header/config.jsx
@@ -0,0 +1,12 @@
+import { FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa'
+import React from 'react'
+
+const SocialShare = [
+  { Social: <FaLinkedinIn />, link: 'https://www.linkedin.com/' },
+  { Social: <FaInstagram />, link: 'https://www.instagram.com/' },
+  { Social: <FaTwitter />, link: 'https://twitter.com/' }
+]
+
+const GithubLink = 'https://github.com/'
+
+export { SocialShare, GithubLink }
